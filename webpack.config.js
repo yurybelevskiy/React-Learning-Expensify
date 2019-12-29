0068a483a37fbfa55598a0856cf8bd6c50522f51
yurybelevskiy <yury.belevskiy@neosoft.ch>
2019-12-29 17:33:47 +0100

    Setup webpack to break .css/.scss into separate styles.css file

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,49 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: "./src/app.js",
-    output: {
-        path: path.join(__dirname, "public"),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [{
-            loader: "babel-loader",
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            use: [
-                "style-loader",
-                "css-loader",
-                "sass-loader"
-            ],
-            test: /\.s?css$/
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, "public"),
-        historyApiFallback: true
-    }
+module.exports = (_, argv) => {
+    const isProduction = argv.mode === 'production';
+
+    return {
+        entry: "./src/app.js",
+        output: {
+            path: path.join(__dirname, "public"),
+            filename: "bundle.js"
+        },
+        module: {
+            rules: [{
+                loader: "babel-loader",
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    //"style-loader",
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "styles.css"
+            })
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, "public"),
+            historyApiFallback: true
+        }
+    };
 };
